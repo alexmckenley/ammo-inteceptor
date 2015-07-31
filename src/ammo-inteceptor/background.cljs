0828391ace0296774ba740e8b6004efc6925aa40
Alex Taylor <alexmckenley@gmail.com>
2015-07-31 10:53:58 -0700

    Generalize js object conversion with js-func

--- src/ammo-inteceptor/background.cljs
+++ src/ammo-inteceptor/background.cljs
@@ -8,6 +8,9 @@
 (defn handler [response]
   (console/log response))
 
+(defn js-func [f]
+  (fn [& rest] (clj->js (apply f (map #(js->clj % :keywordize-keys true) rest)))))
+
 (defn error-handler [{:keys [status status-text]}]
   (.log js/console (str "something bad happened: " status " " status-text)))
 
@@ -15,8 +18,7 @@
   (map (fn [a] (assoc a :name (clojure.string/replace (:name a) "Test-" ""))) headers))
 
 (defn on-before-send-headers [request]
-  (let [newHeaders (js->clj (aget request "requestHeaders") :keywordize-keys true)]
-    (clj->js {:requestHeaders (transform-headers newHeaders)})))
+    {:requestHeaders (transform-headers (:requestHeaders request))})
 
 (defn get-token []
   (GET "https://tpcaahshvs.spotilocal.com:4371/simplecsrf/token.json?ref&cors" {:handle handler
@@ -25,5 +27,5 @@
 
 (defn init []
   (console/log "Requesting token from spotify...")
-  (.addListener js/chrome.webRequest.onBeforeSendHeaders on-before-send-headers (clj->js {:urls ["<all_urls>"]}) #js ["blocking" "requestHeaders"])
+  (.addListener js/chrome.webRequest.onBeforeSendHeaders (js-func on-before-send-headers) (clj->js {:urls ["<all_urls>"]}) #js ["blocking" "requestHeaders"])
   (get-token))
